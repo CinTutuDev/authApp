ead1eb2e05b6679452e48020ba434ff1505e23f3
cinta <cintatafur@gmail.com>
2024-01-25 12:24:03 +0100

    💡 Create(environment and interfaces)

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environments';
+import { Observable, map, of, tap } from 'rxjs';
+import { AuthStatus, User } from '../interfaces';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private readonly baseUrl: string = environment.baseUrl;
+  private http = inject(HttpClient);
+  public _currentUser = signal<User | null>(null);
+  public _authStatus = signal<AuthStatus>(AuthStatus.checking);
+  constructor() {}
 
-  constructor() { }
+  login(email: string, password: string): Observable<boolean> {
+    return of(true);
+  }
 }
